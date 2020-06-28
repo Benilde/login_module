5ca463c3a7398d6670ed0ad5f67b873d9dd2e6df
Carina Matimbe <benilde.matimbe@gmail.com>
2020-06-28 17:46:49 +0200

    updating login page and server request file

---
+++ View/login.php
@@ -0,0 +1,52 @@
+<?php  
+session_start();//session starts here  
+  
+?>  
+  
+<html>  
+<head lang="en">  
+    <meta charset="UTF-8">  
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Login</title>  
+</head>  
+<style>  
+    .login-panel {  
+        margin-top: 150px;  
+  
+</style>  
+  
+<body>  
+  
+<div class="container">  
+    <div class="row">  
+        <div class="col-md-4 col-md-offset-4">  
+            <div class="login-panel panel panel-success">  
+                <div class="panel-heading">  
+                    <h3 class="panel-title">Sign In</h3>  
+                </div>  
+                <div class="panel-body">  
+                    <form role="form" method="post" action="login.php">  
+                        <fieldset>  
+                            <div class="form-group"  >  
+                                <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>  
+                            </div>  
+                            <div class="form-group">  
+                                <input class="form-control" placeholder="Password" name="pass" type="password" value="">  
+                            </div>  
+  
+  
+                                <input class="btn btn-lg btn-success btn-block" type="submit" value="login" name="login" >  
+  
+                            <!-- Change this to a button or input when using this as a form -->  
+                          <!--  <a href="index.html" class="btn btn-lg btn-success btn-block">Login</a> -->  
+                        </fieldset>  
+                    </form>  
+                </div>  
+            </div>  
+        </div>  
+    </div>  
+</div>  
+</body>  
+  
+</html>  
+  
